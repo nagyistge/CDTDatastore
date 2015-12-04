b9443a518f04aad80e41f509ea248ea3f821465b
Tom Blench <tom.blench@cloudant.com>
2015-12-04 11:22:44 +0000

    *** empty log message ***

--- Tests/Tests/Encryption/TDBlobStoreEncryptionTests.m
+++ Tests/Tests/Encryption/TDBlobStoreEncryptionTests.m
@@ -677,7 +677,9 @@
     
     NSError *error;
     NSData *data = [reader dataWithError:&error];
-    XCTAssertNotNil(error, @"Error should not be nil, but was %@", error);
+    
+    XCTAssertNil(error, @"Error should not be nil, but was %@", error);
+    NSLog(@"Data was %@", data);
     XCTAssertEqualObjects(self.plainData, data,
                           @"It has to return the same data previously saved");
 }
